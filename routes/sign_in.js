d6f588e253b3cd7a2f795aadaafb4930873c8319
rishabh-malik <rishabhmalik249@gmail.com>
2017-11-24 14:34:34 +0530

    adding routes

---
+++ routes/sign_in.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var path=require('path')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('signIn.html', {root: __dirname+'/public' })
+});
+
+module.exports = router;
