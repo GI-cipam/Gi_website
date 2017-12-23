4bf3cfa3d4f42c1dc9f421b36367ed9abe6fbd99
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-23 10:50:32 +0530

    sending email

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ var server=app.listen(port,function(){
     console.log('listening to request on port 4000')
 });
 
+function email(){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'gi.cipam@gmail.com',
+          pass: 'cipam@123'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'gi.cipam@gmail.com',
+        to: 'rishabhmalik249@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
+
+email();
+
 app.use('/', home);
 app.use('/signIn',signin);
 app.use('/signUp',signup);
